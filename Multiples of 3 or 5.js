8244f6dadbd7e8b81ac9f8b73ad81d9300fda603
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2022-11-17 11:52:28 -0300

    Correct indentation on Multiples of 3 or 5.js

--- Multiples of 3 or 5.js
+++ Multiples of 3 or 5.js
@@ -18,9 +18,9 @@ Courtesy of projecteuler.net (Problem 1)
 function solution(number){
     let sum = 0;
     for (let i = 0; i < number; i++){
-      if (i % 3 === 0 || i % 5 === 0){
+        if (i % 3 === 0 || i % 5 === 0){
         sum += i;
-      }
+        }
     }
     return sum;
-  }
+    }
